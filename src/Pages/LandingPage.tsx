8107734021d7088cf450be22799c50f6b7721a79
Andy Ernst <andycernst@gmail.com>
2023-07-19 17:19:07 +0100

    improve mobile styles on landing page

--- src/Pages/LandingPage.tsx
+++ src/Pages/LandingPage.tsx
@@ -10,7 +10,7 @@ export const LandingPage = () => {
   return (
     <PageLayout
       contained={false}
-      sx={{ backgroundColor: theme => theme.palette.primary.dark, flexGrow: 1 }}
+      sx={{ backgroundColor: (theme) => theme.palette.primary.dark, flexGrow: 1 }}
     >
       <Box component="article" sx={{ position: 'relative' }}>
         <Box
@@ -20,6 +20,10 @@ export const LandingPage = () => {
             width: '45%',
             height: '100%',
             right: 0,
+            display: {
+              xs: 'none',
+              sm: 'block',
+            },
           }}
         >
           <img
@@ -43,58 +47,67 @@ export const LandingPage = () => {
             backgroundColor: 'transparent',
           }}
         >
-          <Stack maxWidth="500px" marginY={6}>
-            <Box component="h1" sx={{ maxWidth: '250px' }}>
-              <ReactSVG
-                src={title}
-                style={{ fill: 'white', height: '100%' }}
-                title="Fair Slice"
-              />
-            </Box>
+          <Stack direction={{ xs: 'column', sm: 'row' }}>
+            <Stack maxWidth="500px" marginY={12}>
+              <Box component="h1" sx={{ maxWidth: '250px' }}>
+                <ReactSVG
+                  src={title}
+                  style={{ fill: 'white', height: '100%' }}
+                  title="Fair Slice"
+                />
+              </Box>
 
-            <Box sx={{ fontSize: '24px' }}>
-              <p>Sharing is Caring.</p>
-              <p>Learn the science of provably fair resource splitting.</p>
-            </Box>
+              <Box sx={{ fontSize: '24px' }}>
+                <p>Sharing is Caring.</p>
+                <p>Learn the science of provably fair resource splitting.</p>
+              </Box>
 
-            <Stack direction="row" alignItems="center" spacing={2}>
-              <ButtonLink variant="outlined" color="inherit" href={'/graph'}>
-                Splitting Tool
-              </ButtonLink>
+              <Stack direction="row" alignItems="center" spacing={2}>
+                <ButtonLink variant="outlined" color="inherit" href={'/graph'}>
+                  Splitting Tool
+                </ButtonLink>
 
-              <ButtonLink variant="contained" href={'/learn'}>
-                Learn
-              </ButtonLink>
+                <ButtonLink variant="contained" href={'/learn'}>
+                  Learn
+                </ButtonLink>
+              </Stack>
             </Stack>
-          </Stack>
 
-          <Stack
-            sx={{
-              margin: 6,
-              marginLeft: {
-                xs: 4,
-                sm: 20,
-              },
-            }}
-            alignItems="center"
-            justifyContent="center"
-            direction="column"
-          >
-            <img
-              src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/53176/birthday-cake-emoji-clipart-md.png"
-              style={{ width: '100%', maxWidth: 500 }}
-              alt=""
-            />
-            <div
-              style={{
-                position: 'relative',
-                top: '-200px',
-                textAlign: 'center',
-                fontSize: 40,
+            <Stack
+              sx={{
+                backgroundColor: { xs: 'white', sm: 'transparent' },
+                paddingY: 6,
+                paddingRight: {
+                  xs: 4,
+                  sm: 2,
+                  md: 6,
+                },
+                paddingLeft: {
+                  xs: 4,
+                  sm: 8,
+                  md: 20,
+                },
               }}
+              alignItems="center"
+              justifyContent="center"
+              direction="column"
             >
-              PLACEHOLDER
-            </div>
+              <img
+                src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/53176/birthday-cake-emoji-clipart-md.png"
+                style={{ width: '100%', maxWidth: 500 }}
+                alt=""
+              />
+              <div
+                style={{
+                  position: 'relative',
+                  top: '-200px',
+                  textAlign: 'center',
+                  fontSize: 40,
+                }}
+              >
+                PLACEHOLDER
+              </div>
+            </Stack>
           </Stack>
         </LayoutContainer>
       </Box>
